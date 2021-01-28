f943008f1b87139cf5746118622c596a86f3e4e8
lukagoderdzishvili <67374477+lukagoderdzishvili@users.noreply.github.com>
2021-01-28 14:25:36 +0400

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,36 @@
+function page1(){
+    var text = "Thermodynamic fluctuations and statistical physics Einstein first paper submitted in 1900 to Annalen der Physik was on capillary attraction. It was published in 1901 with the title Folgerungen aus den Capillaritatserscheinungen, which translates as Conclusions from the capillarity phenomena. Two papers he published in 1902–1903 (thermodynamics) attempted to interpret atomic phenomena from a statistical point of view. These papers were the foundation for the 1905 paper on Brownian motion, which showed that Brownian movement can be construed as firm evidence that molecules exist. His research in 1903 and 1904 was mainly concerned with the effect of finite atomic size on diffusion phenomena.";
+  
+    document.getElementById("title").innerText = document.getElementById("header-link-page1").innerText; 
+    document.getElementById("page1-p").innerText = text;
+}
+function page2(){
+    var text = "In 1935, Einstein collaborated with Nathan Rosen to produce a model of a wormhole, often called Einstein–Rosen bridges. His motivation was to model elementary particles with charge as a solution of gravitational field equations, in line with the program outlined in the paper Do Gravitational Fields play an Important Role in the Constitution of the Elementary Particles?. These solutions cut and pasted Schwarzschild black holes to make a bridge between two patches. If one end of a wormhole was positively charged, the other end would be negatively charged. These properties led Einstein to believe that pairs of particles and antiparticles could be described in this way.";
+
+    document.getElementById("title").innerText = document.getElementById("header-link-page2").innerText; 
+    document.getElementById("page1-p").innerText = text;
+}
+function page3(){
+    var text = "In order to incorporate spinning point particles into general relativity, the affine connection needed to be generalized to include an antisymmetric part, called the torsion. This modification was made by Einstein and Cartan in the 1920s.";
+
+    document.getElementById("title").innerText = document.getElementById("header-link-page3").innerText; 
+    document.getElementById("page1-p").innerText = text;
+}
+function page4(){
+    var text = "The theory of general relativity has a fundamental law—the Einstein field equations, which describe how space curves. The geodesic equation, which describes how particles move, may be derived from the Einstein field equations. Since the equations of general relativity are non-linear, a lump of energy made out of pure gravitational fields, like a black hole, would move on a trajectory which is determined by the Einstein field equations themselves, not by a new law. So Einstein proposed that the path of a singular solution, like a black hole, would be determined to be a geodesic from general relativity itself. This was established by Einstein, Infeld, and Hoffmann for pointlike objects without angular momentum, and by Roy Kerr for spinning objects. ";
+
+    document.getElementById("title").innerText = document.getElementById("header-link-page4").innerText; 
+    document.getElementById("page1-p").innerText = text;
+}
+function page5(){
+    var text = "In a 1905 paper,[185] Einstein postulated that light itself consists of localized particles (quanta). Einstein's light quanta were nearly universally rejected by all physicists, including Max Planck and Niels Bohr. This idea only became universally accepted in 1919, with Robert Millikan's detailed experiments on the photoelectric effect, and with the measurement of Compton scattering. Einstein concluded that each wave of frequency f is associated with a collection of photons with energy hf each, where h is Planck's constant. He does not say much more, because he is not sure how the particles are related to the wave. But he does suggest that this idea would explain certain experimental results, notably the photoelectric effect.";
+
+    document.getElementById("title").innerText = document.getElementById("header-link-page5").innerText; 
+    document.getElementById("page1-p").innerText = text;
+}
+function page6(){
+    var text = "In a series of works completed from 1911 to 1913, Planck reformulated his 1900 quantum theory and introduced the idea of zero-point energy in his second quantum theory. Soon, this idea attracted the attention of Einstein and his assistant Otto Stern. Assuming the energy of rotating diatomic molecules contains zero-point energy, they then compared the theoretical specific heat of hydrogen gas with the experimental data. The numbers matched nicely. However, after publishing the findings, they promptly withdrew their support, because they no longer had confidence in the correctness of the idea of zero-point energy.";
+
+    document.getElementById("title").innerText = document.getElementById("header-link-page6").innerText; 
+    document.getElementById("page1-p").innerText = text;
+}
